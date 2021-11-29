0473f898260cda00edc98f74bfdac8b740748683
Rachna Gupta <rachgupta200@gmail.com>
2021-11-29 15:31:36 -0500

    added sample texts to debug with and added preprocessing for tokenizing words and adding word keys to matrix

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from sys import argv
+import re
+import nltk
+from nltk import tokenize
 
 # Initialize an dictionary for arg inputs
 texts = {}
 
-# Populates the letters
+# Populates a dictionary of full texts
 for i in range(len(argv) - 1):
     text = open(argv[i + 1])
     texts[argv[i + 1]] = text.read()
     text.close()
+
+
+#main dictionary
+matrix = {}
+for file in texts:
+    text = texts[file]
+    #split into sentences (list of sentences)
+    sentences = tokenize.sent_tokenize(text)
+    for sentence in sentences:
+        #remove punctuation and add to matrix
+        sentence = re.sub(r'[^\w\s]', '', sentence)
+        sentence = re.sub(r'\n', '', sentence)
+        sentence_words = re.split(' ', sentence)
+        for word in sentence_words:
+            matrix[word.lower()] = []
