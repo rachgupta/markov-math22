233f1b51ac93aeb50e8e329f1dda3c6d1d519532
Rachna Gupta <rachgupta200@gmail.com>
2021-12-12 12:46:50 -0500

    commented our code

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from collections import Counter
 import random
 import glob
 import os
-from helpers import matrix_mult
 
 #expects first command-line argument to be name of dir with files
 current_path = os.getcwd()
@@ -19,21 +18,15 @@ if(len(argv)>2):
 else:
     num_sentences = 5
 
-#expects third command-line argument to be boolean indicating whether or not first word should be random
+#expects third command-line argument to be list of first_words
 if(len(argv)>3):
-    random_first = bool(argv[3])
-else:
-    random_first = False
-
-#expects fourth command-line argument to be list of first_words
-if(len(argv)>4):
-    literal = argv[4].replace("[","")
+    literal = argv[3].replace("[","")
     literal = literal.replace("]","")
     list_of_first_words = [c for c in literal.split(",")]
 
-#expects fifth command-line argument to be max number of words in a sentence
-if(len(argv)>5):
-    max_words = int(argv[5])
+#expects fourth command-line argument to be max number of words in a sentence
+if(len(argv)>4):
+    max_words = int(argv[4])
 else:
     max_words = 500
 
@@ -56,13 +49,17 @@ for file in texts:
     #split into sentences (list of sentences)
     sentences = tokenize.sent_tokenize(text)
     for sentence in sentences:
-        #remove punctuation and add to matrix
+        #remove punctuation
         sentence = re.sub(r'[^\w\s]', '', sentence)
         sentence = re.sub(r'\n', '', sentence)
+        #split sentence into list of words
         sentence_words = re.split(' ', sentence)
+        #make each word lowercase and add it to the matrix
         for word in sentence_words:
             matrix[word.lower()] = {}
+        #add to greater list of sentences
         all_sentences.append([w.lower() for w in sentence_words])
+
 for word in matrix:
     #finds all the words that follow a given word in the matrix
     following_words = []
@@ -80,6 +77,8 @@ for word in matrix:
         else:
             matrix[word][key] = 0
 
+
+#helper function that we used to validate that our matrix has rows adding up to 1
 def validate_rows(matrix):
     numRight = 0
     numWrong = 0
@@ -95,6 +94,8 @@ def validate_rows(matrix):
     print("right: " + str(numRight))
     print("wrong: " + str(numWrong))
 
+
+#uses probability from given dictionary of words to randomly choose a word
 def roulette(words):
     random.seed()
     #plug weights and candidates into random choice function
@@ -103,6 +104,8 @@ def roulette(words):
     for candidate in words.keys():
         weights.append(words[candidate])
         candidates.append(candidate)
+    
+    #if the word doesn't have anything following it in any instance, return end of sentence punctuation
     if(sum(weights)==0):
         return "."
     choice = random.choices(candidates, weights=weights, k=1)
@@ -111,18 +114,25 @@ def roulette(words):
 
 def markov_chain(stochastic, first_word,max_words):
     sentence = first_word.capitalize()
+    # get the probabilities for that first word (x1)
     running_vector = stochastic[first_word]
+    #initialize the current_word variable with the first word given
     current_word = first_word
+    #repeat for each word in the sentence
     for i in range(max_words):
+        #use the weights to choose a new word
         current_word = roulette(running_vector)
+        #end the sentence if the new word indicates a natural end to the sentence
         if(current_word=="."):
             return sentence + "."
+        #if not, add the new sentence
         sentence = sentence + " " + current_word
+        #Xn = P * X(n-1)
+        #update Xn using stochastic matrix and current X(n-1)
         running_vector = matrix_mult(stochastic, list(running_vector.values()))
-        #check if word is end of sentence punctuation
-        # if so break loop
-        # if not continue
     return sentence
+
+#generate random sentence using just the word before and the stochastic matrix
 def generate_random_sentence(matrix, first_word,max_words):
     sentence = first_word.capitalize()
     current_word = first_word
@@ -136,11 +146,13 @@ def generate_random_sentence(matrix, first_word,max_words):
         if(current_word=="."):
             return sentence
     return sentence
+
 for i in range(num_sentences):
     print(generate_random_sentence(matrix, list_of_first_words[i],max_words))
     #print(markov_chain(matrix, list_of_first_words[i],max_words))
     print("")
-    #print("done")
+
+#helper function to do matrix multiplication
 def matrix_mult(matrix, vector):
     #matrix will be dictionary of dictionaries
     #vector will be list
--- make_txt_files.py
+++ make_txt_files.py
@@ -2,14 +2,17 @@ import os;
 from sys import argv;
 import glob;
 
+#expects one command-line argument
 current_path = os.getcwd()
 new_path = current_path + "\\" + argv[1]
 os.chdir(new_path)
 
-
+#helper function to clean transcript files when copied directly from Panopto and create new files ready for main.py
 texts = {}
 all_txt_files = glob.glob('*.txt')
 for i in range(len(all_txt_files)):
+    #creates new file in other folder with every other line of og file
+    #because panopto has timestamps every other line
     inputFile = open(all_txt_files[i],'r')
     new_file_path = current_path + "\\new_lectures\\" + all_txt_files[i]
     outputFile = open(new_file_path,'x')
@@ -18,5 +21,6 @@ for i in range(len(all_txt_files)):
         if (count):
             outputFile.write(line)
         count = not count
+    #closes files
     inputFile.close()
     outputFile.close()
